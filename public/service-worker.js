74d4c680f8c5d619c277ec759cb71ba888e1529e
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-09-18 17:40:00 -0500

    created life cycle install fuction

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,15 @@ const FILES_TO_CACHE = [
     '/index.js',
     '/style.css'
 ];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches
+            .open(STATIC_CACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
