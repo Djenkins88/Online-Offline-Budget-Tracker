7b52f558e633d807d463aa8a6f0ffe75c814e339
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-09-19 17:39:02 -0500

    added dotenv.config to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config();
 
 const PORT = 3000;
 
@@ -9,13 +10,13 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(compression());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
